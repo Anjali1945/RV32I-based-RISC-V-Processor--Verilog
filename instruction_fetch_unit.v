f893369bbedc517d3209b26225a5c36b8085f487
Anjali1945 <anjalidt1945@gmail.com>
2026-07-19 12:57:58 +0530

    Update instruction_fetch_unit.v

--- instruction_fetch_unit.v
+++ instruction_fetch_unit.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09.07.2026 10:38:12
-// Design Name: 
-// Module Name: instruction_fetch_unit
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module instruction_fetch_unit(
 input clk, reset,

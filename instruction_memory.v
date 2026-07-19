3de28c055a29182b085cf32be7f6cb35cd5ec76e
Anjali1945 <anjalidt1945@gmail.com>
2026-07-19 12:58:15 +0530

    Update instruction_memory.v

--- instruction_memory.v
+++ instruction_memory.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09.07.2026 10:46:38
-// Design Name: 
-// Module Name: instruction_memory
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module instruction_memory(
 input clk, reset,

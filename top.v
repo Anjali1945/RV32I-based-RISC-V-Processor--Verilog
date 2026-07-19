2f606636c7fbdbce5e9201bcf32e5a43db9f81ce
Anjali1945 <anjalidt1945@gmail.com>
2026-07-19 12:58:56 +0530

    Update top.v

--- top.v
+++ top.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09.07.2026 11:22:40
-// Design Name: 
-// Module Name: top
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module top(
 input clk, reset

98d157bf7f9ac16b0d58fee398a475ff6b98ac99
Anjali1945 <anjalidt1945@gmail.com>
2026-07-19 12:57:04 +0530

    Update control_unit.v

--- control_unit.v
+++ control_unit.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09.07.2026 11:37:57
-// Design Name: 
-// Module Name: control_unit
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module control_unit(
 input reset, 

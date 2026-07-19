79778973b45e92bc8a153f94516e69bbdb64c89e
Anjali1945 <anjalidt1945@gmail.com>
2026-07-19 12:57:21 +0530

    Update data_path_unit.v

--- data_path_unit.v
+++ data_path_unit.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09.07.2026 12:11:21
-// Design Name: 
-// Module Name: data_path_unit
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module data_path_unit(
 input clk, reset,
